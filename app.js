af14070bfac54e874ab06740ca0be4d6a3164325
Hsaadeh <hsaadeh00@gmail.com>
2021-10-07 19:53:37 -0500

    Parallax Effect for Pages
    Added divs and gave them a parallax effect

---
+++ app.js
@@ -0,0 +1,7 @@
+// const parallaxEffect = document.querySelector("#projectNames")
+
+// window.addEventListener("scroll", ()=> {
+//     let offset = window.scrollY;
+
+//     parallaxEffect.style.backgroundPositionY = offset * 1.7 + "px";
+// } )
